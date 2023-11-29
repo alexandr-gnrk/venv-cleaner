f4cefd0f1a12475833d1a1971605f31190962939
Oleksandr Honcharuk <alexandr.gnrk@gmail.com>
2023-11-29 19:15:48 +0200

    Updated and refactored main.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import argparse
-from os import curdir
 from pathlib import Path
-from sys import path
 
 import utils
 
@@ -25,15 +23,43 @@ if not start_dir.is_dir():
 
 dir_gen = utils.child_dirs(start_dir)
 go_down = None  # None value to start generator
+total_size = 0
+freed_size = 0
 while True:
     try:
         curr_dir = dir_gen.send(go_down)
         is_venv = utils.is_virtualenv(curr_dir)
-        if is_venv is True:
-            size = utils.bytes_to_str(utils.get_dir_size(curr_dir))
-            go_down = False
-            print(curr_dir, is_venv, size)
-        else:
+
+        if is_venv is False:
             go_down = True
-    except StopIteration:
+            continue
+        else:
+            go_down = False
+
+        size = utils.get_dir_size(curr_dir)
+        total_size += size
+
+        is_broken = utils.is_broken_venv(curr_dir)
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('Path:', curr_dir)
+        print('Name:', utils.get_venv_name(curr_dir))
+        print('Size:', utils.bytes_to_str(size))
+        print('Is broken:', is_broken)
+
+        action = utils.read_action(backup=not is_broken)
+
+        if action == 'y':  # delete
+            utils.rm_venv(curr_dir)
+            freed_size += size
+        elif action == 'b':  # backup
+            utils.get_freeze(curr_dir)
+            backup_path = utils.create_requirements_backup(curr_dir)
+            print('Requirements backup location:', backup_path)
+            utils.rm_venv(curr_dir)
+            freed_size += size
+        else:  # skip
+            pass
+    except (StopIteration, KeyboardInterrupt):
+        print('Total size:', utils.bytes_to_str(total_size))
+        print('Freed size:', utils.bytes_to_str(freed_size))
         break
